c748256c7abd04326873e5203913b0b04ca5b9fa
AnushaY1916 <anuanusha471@gmail.com>
2024-02-16 11:37:38 +0530

    Improvements
    Signed-off-by: AnushaY1916 <anuanusha471@gmail.com>

--- pkg/driver/driver.go
+++ pkg/driver/driver.go
@@ -171,14 +171,14 @@ func (driver *Driver) Start(nodeService bool) error {
 		driver.grpc = NewNonBlockingGRPCServer()
 		if nodeService {
 			driver.grpc.Start(driver.endpoint, driver, nil, driver)
+			if driver.nodeMonitor != nil {
+				driver.nodeMonitor.StartNodeMonitor()
+			}
 		} else {
 			driver.grpc.Start(driver.endpoint, driver, driver, nil)
 			// start pod monitor along with controller plugin
 			if driver.podMonitor != nil {
 				driver.podMonitor.StartMonitor()
-			}
-			if driver.nodeMonitor != nil {
-				driver.nodeMonitor.StartNodeMonitor()
 			}
 		}
 	}()
